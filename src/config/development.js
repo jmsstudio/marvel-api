0a95c9f6c2fde8be080ca936cb4da5e5d936ede1
Jefferson M Souza <jefferson.souza@synchro.com.br>
2018-01-16 17:54:08 -0200

    adjust on config file

--- src/config/development.js
+++ src/config/development.js
@@ -1,6 +1,7 @@
 module.exports = {
   marvelApi: {
     privateKey: 'PRIVATE_KEY_HERE',
-    publicKey: 'PUBLIC_KEY_HERE'
+    publicKey: 'PUBLIC_KEY_HERE',
+    baseUrl: 'http://gateway.marvel.com/v1/public'
   }
 };
