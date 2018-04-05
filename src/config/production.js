c4e0a148b97a59a3bf82fe32d0f465117e2800b4
jefferson.souza <jefferson.souza@synchro.com.br>
2018-04-05 14:23:29 -0300

    Ajusted to create a production build

---
+++ src/config/production.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+module.exports = {
+  marvelApi: {
+    privateKey: 'PRIVATE_KEY_HERE',
+    publicKey: 'PUBLIC_KEY_HERE',
+    baseUrl: 'https://gateway.marvel.com/v1/public',
+    endpoints: {
+      characters: 'characters',
+      comics: 'comics',
+    },
+  },
+};
