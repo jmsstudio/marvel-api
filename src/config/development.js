2daad9bdc6fab22f60f98ad15953211525107bcd
Jefferson M Souza <jefferson.souza@synchro.com.br>
2018-04-05 22:52:03 -0300

    updated url to work with http on dev environment

--- src/config/development.js
+++ src/config/development.js
@@ -3,7 +3,7 @@ module.exports = {
   marvelApi: {
     privateKey: 'PRIVATE_KEY_HERE',
     publicKey: 'PUBLIC_KEY_HERE',
-    baseUrl: 'http://gateway.marvel.com/v1/public',
+    baseUrl: 'https://gateway.marvel.com/v1/public',
     endpoints: {
       characters: 'characters',
       comics: 'comics',
